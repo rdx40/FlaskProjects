c1f0ca032d9ceaf7df8b40b92ae14a9246135924
rdx40 <ivanjaison@gmail.com>
2024-10-13 20:14:13 +0530

    better version

--- expts/02_most_basic_app/app.py
+++ expts/02_most_basic_app/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "GET":
-        return render_template("get_details.html")
     if request.method == "POST":
-        name = request.form['name']
-        return render_template("display_details.html",display_name = name)
+        name = request.form.get('name')  
+        return render_template("display_details.html", display_name=name)
+    return render_template("get_details.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
