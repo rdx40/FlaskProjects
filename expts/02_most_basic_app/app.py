93a2e91c8edab877d51b8667feba1d7100793308
rdx40 <ivanjaison@gmail.com>
2024-10-13 16:53:03 +0530

    Basic Flask App

---
+++ expts/02_most_basic_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    if request.method == "GET":
+        return render_template("get_details.html")
+    if request.method == "POST":
+        name = request.form['name']
+        return render_template("display_details.html",display_name = name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
